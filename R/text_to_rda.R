45cd712061e048fc15f0b16ad2a351a3f445264e
Tim <timothy.a.myers@gmail.com>
2024-02-15 10:03:07 -0500

    add notes about auto loading rcc_snps at start of project

--- R/text_to_rda.R
+++ R/text_to_rda.R
@@ -9,4 +9,12 @@ my_data <- read.delim("data-raw/rcc_snps.txt", header = FALSE)
 # Save "my_data_ object to a file
 save(my_data, file = "data-raw/rcc_snps.rda")
 
-#
+
+# Have the R project automatically load the list of rcc_snps for use
+# by the `findr` script by adding the lines below to the .Rprofile file.
+
+# if (file.exists("rcc_snps.rda")) {
+#   load("rcc_snps.rda")
+# } else {
+#  message("The file 'rcc_snps.rda' does not exist.")
+# }
