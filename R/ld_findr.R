c1369211eaafc696caf16a0df2001c8e24d46367
Tim <timothy.a.myers@gmail.com>
2024-04-08 16:05:31 -0400

    add write high ld snps to file

--- R/ld_findr.R
+++ R/ld_findr.R
@@ -24,6 +24,21 @@ ld_snps <- read.table("data/data-out/ld/combined_query_snp_list_grch38.txt",
                       sep = "\t",
                       row.names = NULL)
 
+
 # 3) Filter SNPs by R^2
 high_ld_snps <- subset(ld_snps, R2 >=0.9)
 
+
+# 4) Write dataframe "high_ld_snps" to CSV file
+
+# Get current date and time to use for unique filename
+current_time <- format(Sys.time(), "%Y%m%d_%H%M%S")
+
+# Create length of "snp_list" to use in "file_name" below
+rcc_snps_length <- length(rcc.snps)
+
+# Create a unique file name using the current date and time
+file_name <- paste0("data/data-out/ld/high_ld_snps_query", rcc_snps_length, "_", current_time, ".csv")
+
+# Write the dataframe "snp_sequence" to a CSV file called "file_name"
+write.csv(high_ld_snps, file = file_name, row.names = FALSE)
