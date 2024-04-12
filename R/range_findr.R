5b6240d272746acd6fa7c15bb7a71d951fc86cd2
Tim <timothy.a.myers@gmail.com>
2024-04-12 11:44:54 -0400

    minor updates

--- R/range_findr.R
+++ R/range_findr.R
@@ -10,16 +10,15 @@ library(motifbreakR)
 
 # Read in the data generated in step 3 of `findr` script and
 # saved to an RDS file
-gr_results <- readDS("data/data-out/ld/rcc_tfbs_encodemotif_high-ld_r2-0.9_win-10K_20240411_155434.rds")
-
+gr_results <- readRDS("data/data-out/ld/rcc_tfbs_encodemotif_high-ld_r2-0.9_win-10K_granges_object_pval_20240412_113759.rds")
 
 
 ##### Examples of additional analyses #####
 
 # Subset results by rsID
-rs7794538 <- df2gr_results[df2gr_results$SNP_id == "rs7794538"]
+rs7794538 <- gr_results[gr_results$SNP_id == "rs7794538"]
 rs7794538
 
 # Visualize results
-plotMB(results = df2gr_results, rsid = "rs7794538", effect = "strong", altAllele = "C")
+plotMB(results = gr_results, rsid = "rs7794538", effect = "strong", altAllele = "T")
 plotMB(results = rs7794538, rsid = "rs7794538", effect = "strong", altAllele = "T")
